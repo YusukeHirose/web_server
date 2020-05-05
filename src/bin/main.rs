947236d432d0fb8ce635e7455221b4367838cb8d
YusukeHirose <kodecurry@gmail.com>
2020-05-05 19:21:40 +0900

    feat: マルチスレッドでの処理

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,13 @@
+extern crate web_server;
+use web_server::ThreadPool;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
